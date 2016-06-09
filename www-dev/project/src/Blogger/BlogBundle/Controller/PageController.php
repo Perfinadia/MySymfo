b064ca2def6933abc4bb13810764590e07b96793
Perfinadia <max.emilien96@gmail.com>
2016-06-09 06:37:27 -0700

    Mail done

--- www-dev/project/src/Blogger/BlogBundle/Controller/PageController.php
+++ www-dev/project/src/Blogger/BlogBundle/Controller/PageController.php
@@ -8,7 +8,7 @@ use Blogger\BlogBundle\Entity\Enquiry;
 use Blogger\BlogBundle\Form\EnquiryType;
 ///////////////////////////
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class PageController extends Controller
 {
@@ -31,13 +31,18 @@ class PageController extends Controller
         $form->handleRequest($request);
 
         if ($form->isValid()) {
-            // Perform some action, such as sending an email
+             $message = \Swift_Message::newInstance()
+            ->setSubject('Contact enquiry from symblog')
+            ->setFrom('max.emilien96tera2@gmail.com')
+            ->setTo($this->container->getParameter('blogger_blog.emails.contact_email'))
+            ->setBody($this->renderView('BloggerBlogBundle:Page:contactemail.txt.twig', array('enquiry' => $enquiry)));
+            $this->get('mailer')->send($message);
 
+            //$this->get('session')->getFlashBag()->add('blogger-notice', 'Your contact enquiry was successfully sent. Thank you!');
             // Redirect - This is important to prevent users re-posting
             // the form if they refresh the page
             return $this->redirect($this->generateUrl('BloggerBlogBundle_contact'));
         }
-
 
         return $this->render('BloggerBlogBundle:Page:contact.html.twig', array(
             'form' => $form->createView()
